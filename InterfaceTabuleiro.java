e9f581d8c95e7f8c9a8bc1227c36ba8a590427d9
NotHamada <matheushamada@live.com>
2023-04-05 15:02:34 -0300

    Mudança do arranjamento dos botões

--- InterfaceTabuleiro.java
+++ InterfaceTabuleiro.java
@@ -8,6 +8,7 @@ import java.awt.event.ActionEvent;
 
 public class InterfaceTabuleiro extends JFrame implements ActionListener {
     private final JButton[] botoes;
+    private final JButton[][] botoesMatriz;
     private static final String[] nomes = { "um", "dois", "três", "quatro", "cinco", "seis", "sete", "oito" };
     private final Container container;
     private final GridLayout gridLayout1;
@@ -19,28 +20,29 @@ public class InterfaceTabuleiro extends JFrame implements ActionListener {
         setLayout(gridLayout1);
         botoes = new JButton[64];
 
-        for (int i = 0; i < 64; i++) {
-            botoes[i] = new JButton();
-            botoes[i].addActionListener(this);
+        botoesMatriz = new JButton[8][8];
 
-            botoes[i].setBorderPainted(true);
-            botoes[i].setFocusPainted(false);
+        for (int i = 0; i < 8; i++) {
+            for (int j = 0; j < 8; j++) {
+                botoesMatriz[i][j] = new JButton();
+                botoesMatriz[i][j].addActionListener(this);
+                botoesMatriz[i][j].setBorderPainted(true);
+                botoesMatriz[i][j].setFocusPainted(false);
 
-            if(0 <= i%16 && i%16 < 8){
-              if (i % 2 == 0)
-                  botoes[i].setBackground(Color.WHITE);
-              else
-                  botoes[i].setBackground(Color.BLACK);
+                if (i % 2 == 0) {
+                    if (j % 2 == 0)
+                        botoesMatriz[i][j].setBackground(Color.WHITE);
+                    else
+                        botoesMatriz[i][j].setBackground(Color.BLACK);
+                } else {
+                    if (j % 2 == 0)
+                        botoesMatriz[i][j].setBackground(Color.BLACK);
+                    else
+                        botoesMatriz[i][j].setBackground(Color.WHITE);
+                }
+
+                add(botoesMatriz[i][j]);
             }
-            else{
-              if (i % 2 == 1)
-                  botoes[i].setBackground(Color.WHITE);
-              else
-                  botoes[i].setBackground(Color.BLACK);
-            }
-
-            add(botoes[i]);
-
         }
     }
 
