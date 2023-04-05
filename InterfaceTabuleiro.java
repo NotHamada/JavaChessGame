a1be8af3218a7177b68fb002b0f61e673b271a67
NotHamada <matheushamada@live.com>
2023-04-05 14:50:22 -0300

    Inicio da interface do tabuleiro

---
+++ InterfaceTabuleiro.java
@@ -0,0 +1,42 @@
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import java.awt.GridLayout;
+import java.awt.Color;
+import java.awt.Container;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
+public class InterfaceTabuleiro extends JFrame implements ActionListener {
+    private final JButton[] botoes;
+    private static final String[] nomes = { "um", "dois", "três", "quatro", "cinco", "seis", "sete", "oito" };
+    private final Container container;
+    private final GridLayout gridLayout1;
+
+    public InterfaceTabuleiro() {
+        super("Demonstração GridLayout");
+        gridLayout1 = new GridLayout(8, 8); // 2 por 3; gaps de 5
+        container = getContentPane();
+        setLayout(gridLayout1);
+        botoes = new JButton[64];
+
+        for (int i = 0; i < 64; i++) {
+            botoes[i] = new JButton();
+            botoes[i].addActionListener(this);
+
+            botoes[i].setBorderPainted(true);
+            botoes[i].setFocusPainted(false);
+
+            if (i % 2 == 0)
+                botoes[i].setBackground(Color.WHITE);
+            else
+                botoes[i].setBackground(Color.BLACK);
+
+            add(botoes[i]);
+        }
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent event) {
+        container.validate(); // rearranja os elementos do container
+    }
+}
--- Main.java
+++ Main.java
@@ -1,7 +1,14 @@
 import java.util.Scanner;
 
+import javax.swing.JFrame;
+
 public class Main {
     public static void main(String[] args) {
+        InterfaceTabuleiro janela = new InterfaceTabuleiro();
+        janela.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        janela.setSize(300, 200);
+        janela.setVisible(true);
+
         Tabuleiro tabuleiro = new Tabuleiro();
         tabuleiro.inicializaPosicao();
 
