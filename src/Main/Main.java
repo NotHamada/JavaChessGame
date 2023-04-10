c5cadd80f579611b94e26536ab8fc8d175f77ffb
William Vernaschi <williamdavi13@gmail.com>
2023-04-10 02:02:36 -0300

    gui funcionando

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -18,27 +18,5 @@ public class Main {
         Controller controlador = new Controller(tabuleiro, interfaceTabuleiro);
         controlador.iniciaJogo();
 
-
-
-
-
-        System.out.println(tabuleiro.toString());
-
-        Scanner scan = new Scanner(System.in);
-        while (scan.hasNext()) {
-            String from = scan.next();
-            String to = scan.next();
-
-            try {
-                tabuleiro.moverPeca(from, to);
-            } catch (MovementNotAllowedException e) {
-                System.out.println(e.getMessage());
-            }
-
-            System.out.println(tabuleiro.toString());
-
-        }
-        scan.close();
-
     }
 }
